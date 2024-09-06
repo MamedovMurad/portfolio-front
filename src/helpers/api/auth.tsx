4638f6268584d114e5f36f5ea0b7d989852eeba0
MamedovMurad <muradvm@code.edu.az>
2024-09-06 14:07:10 -0700

    add google api

--- src/helpers/api/auth.tsx
+++ src/helpers/api/auth.tsx
@@ -6,7 +6,7 @@ const Login = (params: { email: string, password: string }) => {
 }
 const GoogleLoginPost = (params: { token: any }) => {
     
-    return api.get('auth/callback/google?token='+params.token, params)
+    return api.get('auth/google/callback?token='+params.token, params)
 }
 
 const Register = (params: { email: string, name: string, password: string, password_repeat: string, surname: string }) => {
