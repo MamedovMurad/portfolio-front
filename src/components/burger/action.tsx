7c6e57dda2c718850f3b92af85156876f638a9e0
MamedovMurad <muradvm@code.edu.az>
2024-09-19 00:37:58 -0700

    addded new edit button

--- src/components/burger/action.tsx
+++ src/components/burger/action.tsx
@@ -1,7 +1,7 @@
 import { useDisclosure } from '@mantine/hooks';
 import {  Menu } from '@mantine/core';
 import { DeletePopUp } from '../pop-up/delete';
-import { IconDots } from '@tabler/icons-react';
+import { IconDots, IconEdit } from '@tabler/icons-react';
 
 export function BurgerMenuPortfolio({deleteItem}:{deleteItem?:any}) {
   const [opened, { toggle }] = useDisclosure();
@@ -13,15 +13,15 @@ if (opened) {
   
   <Menu shadow="md" width={80} onClose={toggle}>
       <Menu.Target >
-      <IconDots color="black" className=" bg-black bg-opacity-5 cursor-pointer"/>
+      <IconDots color="black" className=" bg-text-primary cursor-pointer rounded shadow"/>
 
       </Menu.Target>
 
       <Menu.Dropdown>
      
-        {/* <Menu.Item >
-          <Link to={'/dashboard'} className=' text-center block '>Edit</Link>
-        </Menu.Item> */}
+        <Menu.Item className=' w-full bg-text-primary bg-opacity-20 text-text-primary mb-1' >
+         <button  className='w-full flex justify-center h-full '> <IconEdit size={20}/></button>
+        </Menu.Item>
         <Menu.Item className=' bg-red-200 text-red-800'>
             <DeletePopUp deleteItem={deleteItem}/>
         </Menu.Item>
