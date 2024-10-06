b9d96642fdd5fb582630ab202539e0cfec8c27dc
MamedovMurad <muradvm@code.edu.az>
2024-10-06 09:17:01 -0700

    fixed responsive issue in pdf view

--- src/components/modal/showCertificate.tsx
+++ src/components/modal/showCertificate.tsx
@@ -21,12 +21,17 @@ useEffect(() => {
 
     return (
         <>
-            <Modal size={'xl'} opened={opened} onClose={closeAll} title="Certificates" >
-            <div className="relative h-0 pb-[56.25%] overflow-hidden">
+            <Modal size={"xl"} opened={opened} onClose={closeAll} title="Certificates" >
+            <div className="relative w-full overflow-hidden" style={{ paddingTop: "80%" }}>
       <iframe
         src={src}
         title="PDF Viewer"
-        className="absolute top-0 left-0 w-full h-full border-none block md:min-h-[500px] sm:min-h-[300px]"
+        className="absolute top-0 left-0 w-full h-full border-none"
+        style={{
+          height: "100vh", // Ensures full height on mobile
+          width: "100%",   // Full width
+          overflow: "hidden", // Hides any potential overflow
+        }}
       />
     </div>
                 
